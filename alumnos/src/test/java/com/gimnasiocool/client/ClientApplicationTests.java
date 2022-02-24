1cba4a47a08c83c8aa460d358319d1966b08d884
De Jesus Paulo <paulo.max.dj06@gmail.com>
2022-02-24 15:58:55 -0300

    Refactor Application Test Class, change name to ClientApplication Test

---
+++ alumnos/src/test/java/com/gimnasiocool/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gimnasiocool.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
