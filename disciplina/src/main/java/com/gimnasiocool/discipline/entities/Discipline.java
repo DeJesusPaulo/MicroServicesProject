1351f8c78597ab61531b4076fe5a6cd790420161
De Jesus Paulo <paulo.max.dj06@gmail.com>
2022-02-24 17:35:09 -0300

    Refactor getters and setters

--- disciplina/src/main/java/com/gimnasiocool/discipline/entities/Discipline.java
+++ disciplina/src/main/java/com/gimnasiocool/discipline/entities/Discipline.java
@@ -3,38 +3,29 @@ package com.gimnasiocool.discipline.entities;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "tbl_disciplinas")
-@Setter
+@Setter @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Discipline {
 
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+	
+	@Column(name = "name", nullable = false)
+    @NotEmpty(message = "Debe ingresar un nombre")
     private String nombre;
+	
+	@Column(name = "description", nullable = false, length = 120)
     private String descripcion;
+	
+	@Column(name = "status")
     private String status;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long getId() {
-        return id;
-    }
-
-    @Column(name = "name", nullable = false)
-    public String getNombre() {
-        return nombre;
-    }
-
-    @Column(name = "description")
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    @Column(name = "status")
-    public String getStatus() {
-        return status;
-    }
+    
 }
